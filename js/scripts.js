9b6ae6e08800f6ab5e7f806197a776183fa4582a
iamalviaringo <iamalvinaringo@gmail.com>
2016-12-02 12:12:18 +0300

    Specifications 3, 4 and 5 compeleted
    I have replaced numbers divisible by 3, 5 and 15 by "ping",
    "pong" and "pingpong" respectively.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,16 @@ range.push(i);
 }
 
 alert(range);
+
+for(index=0;index<=range.length;index++){
+	if(range[index]%15 === 0) {
+  	range.splice(index, 1, "pingpong");
+  } else if(range[index]%5 === 0) {
+  	range.splice(index, 1, "pong");
+  } else if(range[index]%3 === 0) {
+  	range.splice(index, 1, "ping");
+  };
+};
+
+
+alert(range);
